d745981d3fd4dc5df12f6517f559e2228ef48fc0
hoangminhprovjp2k6 <119112946+hoangminhprovjp2k6@users.noreply.github.com>
2022-11-26 20:01:25 +0700

    party 2

---
+++ buoi 8/mmb.js
@@ -0,0 +1,16 @@
+
+// function hoTen(ho,ten){
+//     alert("Xin chào"+ho+ten)
+// }
+//  hoTen("Nguyễn","Minh")
+
+ //var myfunction = function (ho,ten){
+    //alert("Xin chào"+ho+ten)
+//}
+//myfunction("minh","nguyen")
+var chao = "Chào bạn!";
+function xinChao(){
+    alert(chao)
+}
+// alert(chao)
+xinChao()
